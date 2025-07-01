63d73f53d757af89617585df20feb54280c6762e
TAGARAM MAHESH BABU <thagarammahesh0220@gmail.com>
2025-07-01 10:38:56 +0530

    Update max.py

--- max.py
+++ max.py
@@ -1,6 +1,12 @@
-a=[1,2,6,7,3,4]
-max=a[0]
+a = [1, 2, 7, 8, 9, 5]
+max1 = a[0]
 for i in range(len(a)):
-    if (a[i]>max):
-        max=a[i]
-print(max)
+    if a[i] > max1:
+        max1 = a[i]
+print("first max:", max1)
+max2 = 0
+for i in range(len(a)):
+    if a[i] != max1:
+        if max2 is 0 or a[i] > max2:
+            max2 = a[i]
+print("Second max:", max2)
